d749dd58bf9abe30fd7d85a70955714bf06465df
qkrxo <qkrxo@192.168.0.100>
2020-12-17 18:33:54 +0900

    201217 - tiles 수정

--- src/main/java/kosta/pro/rgmall/controller/MainController.java
+++ src/main/java/kosta/pro/rgmall/controller/MainController.java
@@ -1,8 +1,31 @@
 package kosta.pro.rgmall.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import kosta.pro.rgmall.domain.RegisterGoods;
+import kosta.pro.rgmall.service.MainService;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequestMapping("/main")
+@RequiredArgsConstructor
 public class MainController {
 
+	private final MainService mainService;
+	
+	
+	@RequestMapping("/goodsList/{main}/{sub}/{sort}")
+	public ModelAndView goodsList(@PathVariable int main, @PathVariable int sub, @PathVariable int sort) {
+		
+		//List<RegisterGoods> registerGoodsList = mainService.selectAllGoods(main, sub, sort);
+		
+		ModelAndView mv = new ModelAndView("main/goodsList");
+		return mv;
+	}
+	
 }//class
