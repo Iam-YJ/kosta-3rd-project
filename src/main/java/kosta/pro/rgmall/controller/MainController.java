90823b3c0715e07170b7c2c10893da32697e2f53
Iam-YJ <59785752+Iam-YJ@users.noreply.github.com>
2020-12-17 18:36:32 +0900

    yujin mainController

--- src/main/java/kosta/pro/rgmall/controller/MainController.java
+++ src/main/java/kosta/pro/rgmall/controller/MainController.java
@@ -1,8 +1,38 @@
 package kosta.pro.rgmall.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import kosta.pro.rgmall.domain.UserList;
+import kosta.pro.rgmall.service.MainService;
 
 @Controller
+@RequestMapping("/main")
 public class MainController {
+	private MainService mainService;
 
+	@RequestMapping("/")
+	public String main(){
+		
+		return "main/login";
+	}
+
+	@RequestMapping(value= "/kakaoLogin/{userNick}/{userEmail}")
+	public String kakaoLogin(@PathVariable String userEmail,@PathVariable String userNick) {
+
+	        
+//	        System.out.println("userNick : " + userNick);
+//	        System.out.println("userEmail : " + userEmail);
+			
+			UserList userList = new UserList(userEmail);
+			mainService.userRegister(userList);
+		
+	        ModelAndView mv = new ModelAndView();
+		
+
+	        return "index";
+	}
+	
 }//class
